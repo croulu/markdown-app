bae51a23377c1d6e9e3996a3e20e9f5ae8a1c535
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-16 12:17:11 +0200

    markkdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {sampleText} from "./sampleText";
+
+class App extends Component {
+    state = {
+        text: sampleText
+    }
+
+    handleChange = event => {
+        const text = event.target.value
+        this.setState({text})
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                    <textarea onChange={this.handleChange}
+                        value={this.state.text}
+                        className="form-control"
+                        rows="35">
+                    </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <div>{sampleText}</div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
 }
 
 export default App;
---
+++ src/sampleText.js
@@ -0,0 +1,1 @@
+export const sampleText = '#Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar  une ligne vide.\n\nLaissez deux espaces à la fin d\'une ligne pour \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`,   '
