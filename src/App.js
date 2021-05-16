4586b9173f6125ed6029e055f3ae4bbbd08f1382
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-16 12:57:01 +0200

    marked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css';
+
+import marked from 'marked'
 
 import {sampleText} from "./sampleText";
 
@@ -13,19 +15,24 @@ class App extends Component {
         this.setState({text})
     }
 
+    renderText = text => {
+        const __html =  marked(text, {sanitize: true});
+        return { __html }
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6">
                     <textarea onChange={this.handleChange}
-                        value={this.state.text}
-                        className="form-control"
-                        rows="35">
+                              value={this.state.text}
+                              className="form-control"
+                              rows="35">
                     </textarea>
                     </div>
                     <div className="col-sm-6">
-                        <div>{sampleText}</div>
+                        <div dangerouslySetInnerHTML={ this.renderText(this.state.text) }></div>
                     </div>
                 </div>
             </div>
